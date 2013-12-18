a1cc7109c76bed6d985c6a25afd205954f67c1bf
Andreas Lindh <andreas@superblock.se>
2013-12-18 15:41:13 +0100

    added check_netbackup_tapedrives.sh

---
+++ check_netbackup_tapedrives.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+#
+# check_netbackup_tapedrives.sh
+#
+# Check if last NetBackup catalog backup is older then X hours
+#
+# Author: Andreas Skarmutsos Lindh <andreas@innovationgroup.se>
+#
+# Takes two arguments. Warning threshold and critical threshold.
+#
+
+RC=0
+PROGNAME=`basename $0`
+PROGPATH=`echo $0 | sed -e 's,[\\/][^\\/][^\\/]*$,,'`
+. $PROGPATH/utils.sh
+
+THRESHOLD_WARNING=$1
+THRESHOLD_CRITICAL=$2
+
+if [ $THRESHOLD_WARNING -gt $THRESHOLD_CRITICAL ]; then
+    RC=$STATE_CRITICAL
+    echo "ERROR: Warning threshold cannot be higher than critical threshold"
+    exit $RC
+fi
+
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 <warning_threshold> <critical_treshold>"
+    exit $STATE_UNKNOWN
+fi
+
+SUDOBIN=$(which sudo)
+GREPBIN=$(which grep)
+AWKBIN=$(which awk)
+SEDBIN=$(which sed)
+
+VMOPRCMDBIN=/usr/openv/volmgr/bin/vmoprcmd
+TPCONFIGBIN=/usr/openv/volmgr/bin/tpconfig
+
+OUTPUT=`$TPCONFIGBIN -dl|$AWKBIN '/Status/ {print $2}'`
+
+DRIVES_TOTAL=0
+DRIVES_UP=0
+DRIVES_DOWN=0
+
+# Count ups and downs and totals
+for DRIVE_STATUS in $OUTPUT; do
+    DRIVES_TOTAL=$(($DRIVES_TOTAL+1))
+    if [ $DRIVE_STATUS = "UP" ]; then
+        DRIVES_UP=$(($DRIVES_UP+1))
+    elif [ $DRIVE_STATUS == "DOWN" ]; then
+        DRIVES_DOWN=$(($DRIVES_DOWN+1))
+    fi
+done
+
+if [ $DRIVES_DOWN -eq 0 ]; then
+    RC=$STATE_OK
+    echo "OK: No drives are down"
+elif [ $DRIVES_DOWN -gt $THRESHOLD_CRITICAL ]; then
+    RC=$STATE_CRITICAL
+    echo "CRITICAL: $DRIVES_DOWN drives are down"
+elif [ $DRIVES_DOWN -gt $THRESHOLD_WARNING ]; then
+    RC=$STATE_WARNING
+    echo "WARNING: $DRIVES_DOWN drives are down"
+fi
+
+exit $RC
