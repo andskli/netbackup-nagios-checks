72c237deb460887bd28847e6c02db9f3510fdc5d
Andreas Lindh <andreas@superblock.se>
2013-12-19 10:05:18 +0100

    tapesinscratchpool.sh first stage done

--- check_netbackup_tapesinscratchpool.sh
+++ check_netbackup_tapesinscratchpool.sh
@@ -9,7 +9,7 @@
 # Will warn when 5 tapes left and move to critical when zero tapes are
 # left in scratch pool
 #
-# Author: Andreas Lindh <andreas@innovationgropu.se>
+# Author: Andreas Lindh <andreas@innovationgroup.se>
 #
 
 RC=0
@@ -19,6 +19,13 @@ PROGPATH=`echo $0 | sed -e 's,[\\/][^\\/][^\\/]*$,,'`
 
 THRESHOLD_WARNING=$1
 THRESHOLD_CRITICAL=$2
+
+AWKBIN=$(which awk)
+VMPOOLBIN=/usr/openv/volmgr/bin/vmpool
+VMQUERYBIN=/usr/openv/volmgr/bin/vmquery
+
+MASTERSERVER=`uname -n`
+SCRATCH_COUNT=0
 
 if [ $THRESHOLD_CRITICAL -gt $THRESHOLD_WARNING ]; then
     RC=$STATE_UNKNOWN
@@ -31,6 +38,28 @@ if [ $# -gt 2 ]; then
     exit $STATE_UNKNOWN
 fi
 
-AWKBIN=$(which awk)
+SCRATCH_POOLS=`$VMPOOLBIN -list_scratch|$AWKBIN '!/(^Scratch Pools$|^============.*$)/'`
+set $SCRATCH_POOLS
+if [ $# -lt 1 ]; then
+    echo "UNKNOWN: No scratch pools found"
+    exit $STATE_UNKNOWN
+fi
 
+# Summarize number of scratch tapes
+for pool in $SCRATCH_POOLS; do
+    count=$(expr `$VMQUERYBIN -h $MASTERSERVER -pn $pool -b|wc -l` - 3)
+    SCRATCH_COUNT=$(($SCRATCH_COUNT+$count))
+done
 
+if [ $SCRATCH_COUNT -lt $THRESHOLD_WARNING ]; then
+    RC=$STATE_WARNING
+    echo "WARNING: Only $SCRATCH_COUNT scratch tapes left"
+elif [ $SCRATCH_COUNT -lt $THRESHOLD_CRITICAL ]; then
+    RC=$STATE_CRITICAL
+    echo "CRITICAL: NO scratch tapes left"
+else
+    RC=$STATE_OK
+    echo "OK: $SCRATCH_COUNT scratch tapes left"
+fi
+
+exit $RC
