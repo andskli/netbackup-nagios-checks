f82c05c01319112565a86b4137ad1c7be7f44130
Andreas Lindh <andreas@superblock.se>
2014-01-08 13:46:29 +0100

    check_nbu_pdprocessqueue.sh initial commit

---
+++ check_nbu_pdprocessqueue.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# check_nbu_pdprocessqueue.sh
+#
+# Author: Andreas Skarmutsos Lindh <andreas@innovationgroup.se>
+#
+# Check status of NetBackup PureDisk processqueue
+# Takes two arguments. Warning threshold and critical treshold, in that
+# specific order.
+#
