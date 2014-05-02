0d5be455b0facc0c0d2fb86e163d2896ffb3c916
Andreas Lindh <andreas@superblock.se>
2014-05-02 16:51:09 +0200

    fix pdstatus check

--- check_nbu_pdstatus.sh
+++ check_nbu_pdstatus.sh
@@ -7,13 +7,10 @@
 # Check status of NetBackup PureDisk. Make sure GET/PUT is OK.
 #
 
-RC=0
 PROGNAME=`basename $0`
 PROGPATH=`echo $0 | sed -e 's,[\\/][^\\/][^\\/]*$,,'`
 . $PROGPATH/utils.sh
-
-THRESHOLD_WARNING=$1
-THRESHOLD_CRITICAL=$2
+RC=0
 
 AWKBIN=$(which awk)
 CRCONTROLBIN=/usr/openv/pdde/pdcr/bin/crcontrol
@@ -23,7 +20,7 @@ CRCONTROLSTATUSES=`$CRCONTROLBIN --getmode | $AWKBIN '{print $3" "$4}'`
 for s in $CRCONTROLSTATUSES; do
 	getorput=$(echo $s|$AWKBIN -F'=' '{print $1}')
 	yesorno=$(echo $s|$AWKBIN -F'=' '{print $2}')
-	if [ "No" -eq "$yesorno" ]; then
+	if [ "${yesorno}" == "No" ]; then
 		echo "CRITICAL: $getorput is in No state"
 		RC=$STATE_CRITICAL
 	fi
